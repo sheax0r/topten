57f719d4ad6e79eeadce64a373b695feaae58edc
Mike Shea <michael.shea@salesforce.com>
2015-04-22 16:19:26 -0400

    * Simplify twitter_stream.run method. * Fix off-by-one errors in twitter_stream.next_message method. * Spec tests for twitter_stream

--- lib/topten/twitter_stream.rb
+++ lib/topten/twitter_stream.rb
@@ -24,32 +24,23 @@ module Topten
       http.request(request) do |response|
         response.read_body do |chunk|
           stream_buffer.concat chunk.split(//)
-          loop do
-            message = next_message(stream_buffer)
-            if message
-              yield message
-            else 
-              break
-            end
+          while (message = next_message stream_buffer)
+            yield message
           end
         end
       end
     end
 
-    private
-
     # Parse the next message from the buffer.
     def next_message(stream_buffer)
+      # Trim newlines
+      while stream_buffer[0..1].join == "\r\n"
+        stream_buffer.shift 2
+      end
+
       # Find the line break after the length header.
       # Bail if there isn't one in the current buffer.
       line_break = stream_buffer.index("\r")
-      return nil if line_break.nil?
-
-      # Trim "keep-alive" newlines
-      while (line_break == 0) 
-        stream_buffer.shift 2
-        line_break = stream_buffer.index("\r")
-      end
       return nil if line_break.nil?
 
       # Get the length header.
@@ -58,15 +49,15 @@ module Topten
 
       # Bail if remaining buffer size is less than message length,
       # (include extra space for linebreak and length header).
-      return nil if (stream_buffer.size - msg_length_string.size - 2) <= msg_length 
+      return nil if (stream_buffer.size - msg_length_string.size - 2) < msg_length 
 
       # Extract the message
       msg_start = line_break + 2
-      msg_end = msg_start + msg_length - 2
+      msg_end = msg_start + msg_length - 1
       msg = stream_buffer[msg_start..msg_end].join
 
       # Move the buffer along, return the message
-      stream_buffer.shift msg_end + 2
+      stream_buffer.shift msg_end + 1
       msg
     end
   end
---
+++ spec/twitter_stream_spec.rb
@@ -0,0 +1,89 @@
+require 'spec_helper'
+require 'topten/twitter_stream'
+
+module Topten
+  describe TwitterStream do
+    describe '#run' do
+      let(:oauth_yml) { double 'oauth.yml' }
+      let(:credentials) { double 'credentials' }
+      let(:http) { double('http') }
+      let(:request) { double('request') }
+      let(:response) { double('response') }
+      let(:chunk) { 'chunk' }
+
+      before :each do
+        allow(File).to receive(:read).with('oauth.yml') { oauth_yml }
+        allow(YAML).to receive(:load).with(oauth_yml) { credentials }
+        allow(Net::HTTP).to receive(:new).with('stream.twitter.com', 443) { http }
+        allow(http).to receive(:use_ssl=).with(true)
+        allow(http).to receive(:verify_mode=).with(OpenSSL::SSL::VERIFY_PEER)
+        allow(subject).to receive(:sign_request).with(request, credentials)
+        allow(http).to receive(:request).with(request) { |_, &block| block.call(response) }
+        allow(response).to receive(:read_body){ |_, &block| block.call(chunk) }
+        allow(Net::HTTP::Get).to receive(:new).with('/1.1/statuses/sample.json?delimited=length') { request }
+
+        # Three messages.
+        allow(subject).to receive(:next_message).and_return(
+          'message1', 
+          'message2',
+          nil)
+      end
+
+      it 'should parse all messages from the current chunk' do
+        expect { |b|
+          subject.run(&b) 
+        }.to yield_successive_args('message1', 'message2')
+      end
+    end
+
+    describe '#next_message' do
+
+      let(:array) { buffer.split(//) }
+
+      def next_message
+        subject.next_message(array)
+      end
+
+      shared_examples 'incomplete message' do
+        it 'returns nil' do
+          expect(next_message).to eq nil
+          expect(array).to eq buffer.split(//)
+        end
+      end
+
+      context 'when there is no line break' do
+        let(:buffer) { '5' }
+        it_behaves_like 'incomplete message'
+      end
+
+      context 'when the content is not as long as the length header' do
+        let(:buffer) { "5\r\nhell" }
+        it_behaves_like 'incomplete message'
+      end
+
+      context 'when there is a complete message' do
+        let(:buffer) { "5\r\nhello" }
+        it 'returns a message' do 
+          expect(next_message).to eq "hello"
+          expect(array).to eq []
+        end
+      end
+
+      context 'when there is data following the message' do
+        let(:buffer) { "5\r\nhello\r\n5\r\nagain" }
+        it 'leaves the data following the message alone' do 
+          expect(next_message).to eq "hello"
+          expect(array).to eq "\r\n5\r\nagain".split(//)
+        end
+      end
+
+      context 'when there are newlines at the start of the chunk' do
+        let(:buffer) { "\r\n\r\n5\r\nhello" }
+        it 'trims the newlines and parses the message' do 
+          expect(next_message).to eq "hello"
+          expect(array).to eq []
+        end
+      end
+    end
+  end
+end
